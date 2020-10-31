bd82b3b8ffd882e65d108f3873e87bfad42c3195
DongJeremy <dongjeremy2020@163.com>
2020-10-31 17:26:05 +0800

    添加功能未完成。

--- app/src/main/java/org/cloud/panzer/fragment/GoodsFragment.java
+++ app/src/main/java/org/cloud/panzer/fragment/GoodsFragment.java
@@ -14,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.youth.banner.Banner;
@@ -24,6 +26,7 @@ import org.cloud.core.base.BaseApplication;
 import org.cloud.core.base.BaseConstant;
 import org.cloud.core.base.BaseFragment;
 import org.cloud.core.base.BaseImageLoader;
+import org.cloud.core.utils.JsonUtils;
 import org.cloud.panzer.R;
 import org.cloud.panzer.adapter.EvaluateGoodsSimpleListAdapter;
 import org.cloud.panzer.adapter.GoodsCommendListAdapter;
@@ -39,6 +42,9 @@ import org.greenrobot.eventbus.Subscribe;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import butterknife.BindView;
 
@@ -201,6 +207,10 @@ public class GoodsFragment extends BaseFragment {
     private ArrayList<VoucherGoodsBean> voucherArrayList;
 
     private final ArrayList<String> goodsImageArrayList = new ArrayList<>();
+    private ArrayList<HashMap<String, String>> specNameArrayList = new ArrayList<>();
+    private ArrayList<HashMap<String, String>> specValueArrayList = new ArrayList<>();
+    private ArrayList<HashMap<String, String>> goodsSpecArrayList = new ArrayList<>();
+    private String[] specString = new String[]{"", ""};
 
     private String shareUrl;
     private String shareText;
@@ -262,20 +272,6 @@ public class GoodsFragment extends BaseFragment {
     }
 
     @Override
-    protected void initListener() {
-
-    }
-
-    @Subscribe
-    public void onGoodsBeanEvent(GoodsBeanEvent event) {
-
-        String jsonString = event.getGoodsBean();
-        initData();
-        initListener();
-        handlerData(jsonString);
-    }
-
-    @Override
     protected void initData() {
     }
 
@@ -294,6 +290,19 @@ public class GoodsFragment extends BaseFragment {
     public void onStop() {
         super.onStop();
         mainBanner.stop();
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Subscribe
+    public void onGoodsBeanEvent(GoodsBeanEvent event) {
+        String jsonString = event.getGoodsBean();
+        initData();
+        initListener();
+        handlerData(jsonString);
     }
 
     private void handlerData(String homeInfoData) {
@@ -366,5 +375,230 @@ public class GoodsFragment extends BaseFragment {
         marketPriceTextView.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
         saleTextView.setText("销量：");
         saleTextView.append(goodsInfoJSONObject.get("goods_salenum").getAsString());
+
+        //满送
+        JsonObject mansongData = mainJsonObject.getAsJsonObject("mansong_info");
+        if (mansongData == null) {
+            manSongLinearLayout.setVisibility(View.GONE);
+        } else {
+            manSongLinearLayout.setVisibility(View.VISIBLE);
+            JsonArray jsonArray = mansongData.getAsJsonArray("rules");
+            JsonObject manSongJsonObject = jsonArray.get(0).getAsJsonObject();
+            temp = "单笔订单满￥" + manSongJsonObject.get("price").getAsString() + "，立减￥" + manSongJsonObject.get("discount").getAsString() + "，送礼品";
+            manSongDescTextView.setText(temp);
+            if (manSongJsonObject.has("goods_image_url")) {
+                BaseImageLoader.getInstance().display(manSongJsonObject.get("goods_image_url").getAsString(), manSongGoodsImageView);
+            }
+        }
+        //代金券
+        if (mainJsonObject.has("voucher")) {
+            voucherArrayList.clear();
+            voucherTextView.setVisibility(View.VISIBLE);
+            temp = mainJsonObject.get("voucher").getAsString();
+//            voucherArrayList.addAll(JsonUtils.json2ArrayList(temp, VoucherGoodsBean.class));
+//            voucherAdapter.notifyDataSetChanged();
+        } else {
+            voucherTextView.setVisibility(View.GONE);
+        }
+        //SpecName
+        if (goodsInfoJSONObject.has("spec_name")) {
+            JsonObject jsonObject = goodsInfoJSONObject.getAsJsonObject("spec_name");
+            for (Map.Entry<String, JsonElement> stringJsonElementEntry : jsonObject.entrySet()) {
+                HashMap<String, String> hashMap1 = new HashMap<>();
+                String key = stringJsonElementEntry.toString();
+                String value = jsonObject.get(key).getAsString();
+                hashMap1.put("id", key);
+                hashMap1.put("value", value);
+                specNameArrayList.add(hashMap1);
+            }
+            for (int i = 0; i < specNameArrayList.size(); i++) {
+                if (i < 2) {
+                    chooseLineView[i].setVisibility(View.VISIBLE);
+                    chooseValueRecyclerView[i].setVisibility(View.VISIBLE);
+                    chooseValueTextView[i].setVisibility(View.VISIBLE);
+                    chooseValueTextView[i].setText(specNameArrayList.get(i).get("value"));
+                }
+            }
+        } else {
+            specTextView[0].setText("默认");
+            specTextView[0].setVisibility(View.VISIBLE);
+        }
+        //specValue
+        if (goodsInfoJSONObject.has("spec_value")) {
+            specValueArrayList = new ArrayList<>();
+            JsonObject jsonObject = goodsInfoJSONObject.getAsJsonObject("spec_value");
+            if (specNameArrayList.size() != 0) {
+                for (int i = 0; i < specNameArrayList.size(); i++) {
+                    String id = specNameArrayList.get(i).get("id");
+                    String value = specNameArrayList.get(i).get("value");
+                    JsonObject object = jsonObject.getAsJsonObject(id);
+                    Iterator iterator = object.entrySet().iterator();
+                    while (iterator.hasNext()) {
+                        HashMap<String, String> hashMap1 = new HashMap<>();
+                        String key = iterator.next().toString();
+                        hashMap1.put("value", object.get(key).getAsString());
+                        hashMap1.put("parent_value", value);
+                        hashMap1.put("parent_id", id);
+                        hashMap1.put("id", key);
+                        specValueArrayList.add(hashMap1);
+                    }
+                }
+            }
+        }
+        //goodsSpec
+        if (goodsInfoJSONObject.has("goods_spec")) {
+            goodsSpecArrayList = new ArrayList<>();
+            JsonObject jsonObject = goodsInfoJSONObject.getAsJsonObject("goods_spec");
+            Iterator iterator = jsonObject.entrySet().iterator();
+            while (iterator.hasNext()) {
+                HashMap<String, String> hashMap1 = new HashMap<>();
+                String key = iterator.next().toString();
+                String value = jsonObject.get(key).getAsString();
+                for (int i = 0; i < specValueArrayList.size(); i++) {
+                    String id = specValueArrayList.get(i).get("id");
+                    if (key.equals(id)) {
+                        String parent_value = specValueArrayList.get(i).get("parent_value");
+                        hashMap1.put("key", key);
+                        hashMap1.put("value", value);
+                        hashMap1.put("content", parent_value + "：" + value);
+                    }
+                }
+                goodsSpecArrayList.add(hashMap1);
+            }
+            for (int i = 0; i < goodsSpecArrayList.size(); i++) {
+                if (i < 2) {
+                    specTextView[i].setVisibility(View.VISIBLE);
+                    specTextView[i].setText(goodsSpecArrayList.get(i).get("content"));
+                    specString[i] = goodsSpecArrayList.get(i).get("key");
+                }
+            }
+        }
+        //specList
+        //noinspection unchecked
+        ArrayList<HashMap<String, String>>[] specArrayList = new ArrayList[2];
+        SpecListAdapter[] specAdapter = new SpecListAdapter[2];
+        temp = mainJSONObject.getString("spec_list");
+        specListArrayList = new ArrayList<>(JsonUtil.json2ArrayList(temp));
+        for (int i = 0; i < specNameArrayList.size(); i++) {
+            if (i < 2) {
+                specArrayList[i] = new ArrayList<>();
+                String value = specNameArrayList.get(i).get("value");
+                for (int j = 0; j < specValueArrayList.size(); j++) {
+                    if (value.equals(specValueArrayList.get(j).get("parent_value"))) {
+                        HashMap<String, String> hashMap = new HashMap<>(specValueArrayList.get(j));
+                        hashMap.put("default", "0");
+                        for (int k = 0; k < goodsSpecArrayList.size(); k++) {
+                            if (goodsSpecArrayList.get(k).get("value").equals(hashMap.get("value"))) {
+                                hashMap.put("default", "1");
+                                break;
+                            }
+                        }
+                        specArrayList[i].add(hashMap);
+                    }
+                }
+            }
+        }
+        isBackBoolean = true;
+        if (specArrayList[0] != null) {
+            for (int i = 0; i < specArrayList[0].size(); i++) {
+                String id = specArrayList[0].get(i).get("id");
+                if (id.equals(specString[0])) {
+                    isBackBoolean = false;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < specArrayList.length; i++) {
+            if (i < 2) {
+                if (specArrayList[i] != null) {
+                    final int positionInt = i;
+                    specAdapter[i] = new SpecListAdapter(specArrayList[i]);
+                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+                    linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+                    chooseValueRecyclerView[i].setLayoutManager(linearLayoutManager);
+                    chooseValueRecyclerView[i].setAdapter(specAdapter[i]);
+                    specAdapter[i].setOnItemClickListener((position, id, value) -> {
+                        if (isBackBoolean) {
+                            if (positionInt == 1) {
+                                specString[positionInt - 1] = id;
+                            } else {
+                                specString[positionInt + 1] = id;
+                            }
+                        } else {
+                            specString[positionInt] = id;
+                        }
+                        refreshSpecData();
+                    });
+                }
+            }
+        }
+        //虚拟物品
+        if (goodsInfoJSONObject.getString("is_virtual").equals("0")) {
+            areaRelativeLayout.setVisibility(View.VISIBLE);
+        } else {
+            areaRelativeLayout.setVisibility(View.GONE);
+        }
+        //店铺信息
+        JSONObject storeInfoJSONObject = new JSONObject(mainJSONObject.getString("store_info"));
+        storeId = storeInfoJSONObject.getString("store_id");
+        memberId = storeInfoJSONObject.getString("member_id");
+        temp = "由 “" + storeInfoJSONObject.getString("store_name") + "” 销售和发货，并享受售后服务";
+        storeNameTextView.setText(storeInfoJSONObject.getString("store_name"));
+        voucherStoreNameTextView.setText(storeNameTextView.getText().toString());
+        storeOwnTextView.setText(storeInfoJSONObject.getString("is_own_shop").equals("1") ? "自营店" : "");
+        jsonObject = new JSONObject(storeInfoJSONObject.getString("store_credit"));
+        jsonObject = new JSONObject(jsonObject.getString("store_desccredit"));
+        storeDescTextView.setText(jsonObject.getString("credit"));
+        storeDescPercentTextView.setText(storeInfoJSONObject.getString("is_own_shop").equals("1") ? "平" : jsonObject.getString("percent_text"));
+        jsonObject = new JSONObject(storeInfoJSONObject.getString("store_credit"));
+        jsonObject = new JSONObject(jsonObject.getString("store_servicecredit"));
+        storeServiceTextView.setText(jsonObject.getString("credit"));
+        storeServicePercentTextView.setText(storeInfoJSONObject.getString("is_own_shop").equals("1") ? "平" : jsonObject.getString("percent_text"));
+        jsonObject = new JSONObject(storeInfoJSONObject.getString("store_credit"));
+        jsonObject = new JSONObject(jsonObject.getString("store_deliverycredit"));
+        storeDeliveryTextView.setText(jsonObject.getString("credit"));
+        storeDeliveryPercentTextView.setText(storeInfoJSONObject.getString("is_own_shop").equals("1") ? "平" : jsonObject.getString("percent_text"));
+        serviceDescTextView.setText(temp);
+        //服务信息
+        if (goodsInfoJSONObject.has("contractlist") && goodsInfoJSONObject.getString("contractlist").contains("{")) {
+            serviceSevDayTextView.setVisibility(View.VISIBLE);
+            serviceQualityTextView.setVisibility(View.VISIBLE);
+            serviceReissueTextView.setVisibility(View.VISIBLE);
+            serviceLogisticsTextView.setVisibility(View.VISIBLE);
+        } else {
+            serviceSevDayTextView.setVisibility(View.GONE);
+            serviceQualityTextView.setVisibility(View.GONE);
+            serviceReissueTextView.setVisibility(View.GONE);
+            serviceLogisticsTextView.setVisibility(View.GONE);
+        }
+        //评价信息
+        jsonObject = new JSONObject(mainJSONObject.getString("goods_evaluate_info"));
+        evaluateDescTextView.setText("好评率 ");
+        evaluateDescTextView.append(jsonObject.getString("good_percent") + "%");
+        evaluateNumberTextView.setText("(");
+        evaluateNumberTextView.append(jsonObject.getString("all") + "人评价)");
+        if (mainJSONObject.getString("goods_eval_list").equals("null")) {
+            evaluateRecyclerView.setVisibility(View.GONE);
+        } else {
+            evaluateRecyclerView.setVisibility(View.VISIBLE);
+            evaluateGoodsArrayList.clear();
+            evaluateGoodsArrayList.addAll(JsonUtil.json2ArrayList(mainJSONObject.getString("goods_eval_list"), EvaluateGoodsBean.class));
+            evaluateGoodsAdapter.notifyDataSetChanged();
+        }
+        //商品推荐
+        commendArrayList.clear();
+        commendArrayList.addAll(JsonUtil.json2ArrayList(mainJSONObject.getString("goods_commend_list"), GoodsCommendBean.class));
+        commendAdapter.notifyDataSetChanged();
+        //选择页面
+        BaseImageLoader.get().display(goodsImageArrayList.get(0), chooseGoodsImageView);
+        chooseNameTextView.setText(nameTextView.getText().toString());
+        chooseMoneyTextView.setText(moneyTextView.getText().toString());
+        chooseStorageTextView.setText("库存：");
+        chooseStorageTextView.append(goodsStorageString);
+        if (!TextUtils.isEmpty(lowerLimitString)) {
+            chooseNumberEditText.setText(lowerLimitString);
+        }
+        chooseNumberEditText.setSelection(chooseNumberEditText.getText().length());
+        haveGoods = !goodsStorageString.equals("0");
     }
 }
